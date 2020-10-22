40ac800ba470d0209ae3e47a7d15cb68da160525
adriellecunhasouza <adrielle.cunha.souza@gmail.com>
2020-10-22 15:56:21 -0300

    novo deploy

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,7 +8,19 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>Hello word</h1>
-	A hora agora é: <%= new java.util.Date() %>
+	<h1>Bem-Vindo</h1>
+	<br><br>
+    
+    <form method = "post"  action = "ola-mundo">
+        Entre com um nome: <input type = "text" name = "nome"/>
+        <input type = "submit" value = "Entrar"/>
+    </form>
+    <br>
+    <%= request.getParameter("erro") %>
+    
+    <br><br><br><br><br>
+    A hora agora é: <%= new java.util.Date() %>
+	
+	
 </body>
 </html>
--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,5 +19,20 @@ public class OlaMundo extends HttpServlet {
 		PrintWriter out = resp.getWriter();
 		out.print("<h1>ALO MUNDO</h1>");
 		out.print("<p>Este é o meu primeiro servlet</p>");
+		
+		String p1 = req.getParameter("nome");
+		
+		//RequestDispatcher rd = null;
+		if(p1.trim().length()==0) {
+			req.getRequestDispatcher("index.jsp?erro=ERRO NO NOME").forward(req, resp);
+			
+		//	out.println("<b>Invalid Login Info.</b><br>");
+         //           rd = req.getRequestDispatcher("/ola-mundo.jsp");
+            //        rd.include(req, resp);
+		}
+		else {
+			req.getRequestDispatcher("benvindo.jsp?nome=" + p1.toUpperCase()).forward(req, resp);
+		}
+		
 	}
 }
